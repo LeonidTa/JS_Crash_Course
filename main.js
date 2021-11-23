3da1088f0a715e5a27f56c0535b30d4caf000f57
LeonidTa <leonid.tahiraj@tutanota.com>
2021-11-23 14:03:49 +0100

    started working on main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+// alert('Test'); --> Should not be used, because it blockes the whole page
+console.log('Hello World'); // Logs in the console
+console.error('This is an error'); // Gives you an error in the console with the line number
+console.warn('Warning!');
+
+//Variables
+//var, let, const
+//var shouldn't be used because it is globally scoped and we have let and const as new variables
+
+let age = 30;
+age = 31;
+console.log('age: ' + age)
