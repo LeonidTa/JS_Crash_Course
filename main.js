bd710074fbd6c5fe6a5987348101ee602c8ee424
LeonidTa <leonid.tahiraj@tutanota.com>
2021-11-23 14:39:23 +0100

    Basic String Methods

--- main.js
+++ main.js
@@ -7,6 +7,41 @@ console.warn('Warning!');
 //var, let, const
 //var shouldn't be used because it is globally scoped and we have let and const as new variables
 
+/*
 let age = 30;
 age = 31;
-console.log('age: ' + age)
+
+console.log('age: ' + age);
+*/
+
+// Datatypes --> Strings, Numbers, Boolean, null, undefined, Symbol
+
+const name = 'John';    //String
+const age = 30;         //Int
+const rating = 4.5;     //decimal -> float / double
+const isCool = true;    //boolean -> true, false
+const x = null;         //null
+const y = undefined;    //undefined
+let z;                  //also undefinded
+
+console.log(typeof z);  //typeof -> shows the type of the variable
+
+//Concatenation
+console.log('My name is ' + name + ' and I am ' + age); //Old way of doing it
+
+//Template String
+console.log(`My name is ${name} and I am ${age}`); // new way of doing it
+
+
+const s = 'Hello World';
+console.log(s.length);                          //length of the String
+console.log(s.toUpperCase());                   //makes the letters uppercase
+console.log(s.substring(0,5));                  //Gives you a part of the String
+console.log(s.substring(0,5).toUpperCase());    //Gives you first 5 letters and makes them uppercase
+console.log(s.split(''));                       //Separates the String into Array
+
+//UseCase for using split method in a String
+const t = 'technology, computers, it, code';
+console.log(t.split(', '));                     //In this case the separator is the comma and space after it. 
+
+
