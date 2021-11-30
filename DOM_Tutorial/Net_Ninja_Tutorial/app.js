aee6abbce98d9a961d09ebf28b7ff690095f8e57
LeonidTa <leonid.tahiraj@tutanota.com>
2021-11-30 22:13:21 +0100

    Events & Event bubbling

--- DOM_Tutorial/Net_Ninja_Tutorial/app.js
+++ DOM_Tutorial/Net_Ninja_Tutorial/app.js
@@ -53,3 +53,45 @@ console.log('book-list next sibling is: ', bookList.previousElementSibling);
 //UseCase example
 //Here i am accessing the previous sibling of booklist and selecting the <p>. Then im changing the html content of the <p>
 bookList.previousElementSibling.querySelector('p').innerHTML += '<br/>Too Cool for everyone else!';
+
+//EVENTS & EVENT LISTENERS
+var h2 = document.querySelector('#book-list h2');
+h2.addEventListener('click', function(e){
+    console.log(e.target);      //Shows you the clicked object as html
+    console.log(e);             //Shows you the properties of the click (example: was the shift-key pressed when you clicked the button?)
+});
+
+//How to remove list elements in a list, by clicking a button
+// 1.) Select the buttons
+var btns = document.querySelectorAll('#book-list .delete');
+// 2.) Convert the var into an Array and use loop foreach to read each button seperately
+Array.from(btns).forEach(function (btn) {
+    btn.addEventListener('click', function (e) {    //Added Eventlistener
+
+        const li = e.target.parentElement;          //Grabbing the Parent element of the button, which is in this case the li
+        
+        li.parentElement.removeChild(li);           //Here you are grabbing the parent of the li, which is the ul and then you are saying that you want to remove the child of ul, which is the li
+
+    });
+});
+
+//How to prevent the link from going to another page
+/*
+const link = document.querySelector('#page-banner a')
+
+link.addEventListener('click', function (e) {
+    e.preventDefault;                           //prevents the opening of a new page when clicking on the link
+    console.log('navigation to ', e.target.textContent, 'was prevented' );
+});
+*/
+
+//EVENT BUBBLING
+//This option below gives you the option to delete any list that is named "delete", regardless when you added the object into the HTML
+const list = document.querySelector('#book-list ul');
+
+list.addEventListener('click', function(e){
+    if(e.target.className == 'delete'){ 
+        const li = e.target.parentElement;
+        list.removeChild(li);               //Does the same as function before.
+    };
+});
