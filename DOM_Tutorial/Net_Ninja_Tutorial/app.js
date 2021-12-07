ef3609b9efd6956adadb8ab862176f5ad788f609
LeonidTa <leonid.tahiraj@tutanota.com>
2021-12-07 15:17:44 +0100

    netninja DOM tutorial almost done

--- DOM_Tutorial/Net_Ninja_Tutorial/app.js
+++ DOM_Tutorial/Net_Ninja_Tutorial/app.js
@@ -94,4 +94,57 @@ list.addEventListener('click', function(e){
         const li = e.target.parentElement;
         list.removeChild(li);               //Does the same as function before.
     };
+}); 
+
+//INTERACTING WITH FORMS
+//adding books
+
+const addForm = document.forms['add-book']; 
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();                         //Prevents the default behavior of the "add" button
+    const value = addForm.querySelector('input[type="text"]').value;  //getting the text that was written
+    console.log(value);
+
+    //create elements
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    //add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+
+    //add classes
+    bookName.classList.add('name');
+    deleteBtn.classList.add('delete');
+
+    //append to DOM 
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+});
+
+//Hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e){
+    if(hideBox.checked){
+        list.style.display = "none";
+    } else {
+        list.style.display = "initial";
+    }
+});
+
+//Search / filter books
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function(e){
+    const term = e.target.value.toLowerCase();
+    const books = list.getElementsByTagName('li');
+    Array.from(books).forEach(function(book){
+        const title = book.firstElementChild.textContent;
+        if(title.toLowerCase().indexOf(term) != -1){
+            book.style.display = 'block';
+        } else {
+            book.style.display = "none";
+        }
+    })
 });
